560630954bba63543fb29a116b5aa61a762270c5
Max Menssen <menssen@cell.uni-hannover.de>
2021-06-14 11:00:52 +0200

    new folder name

---
+++ real_data_example_hoffmann_berger/gpq_c1_m3_nfut.R
@@ -0,0 +1,136 @@
+#-------------------------------------------------------------------------------
+#---------------------- GPQ Lin & Liao Method 3 --------------------------------
+#-------------------------------------------------------------------------------
+
+# Data of Table 4 from Hoffmann & Berger 2011 (Normalized mean RU)
+
+# nmru for Run 1
+run_1 <- c(1.11, 1.00, 1.50, 1.04, 0.929, 1.02, 0.965, 0.973, 1.01, 1.02,
+           1.04, 1.13, 1.03, 1.04, 1.02, 1.04, 0.929, 1.04, 0.947, 0.947)
+
+# nmru for Run 2
+run_2 <- c(1.25, 1.18, 1.52, 1.15, 1.13, 1.03, 1.18, 1.18, 1.21, 1.29,
+           1.16, 1.22, 1.35, 1.28, 1.29, 1.38, 1.20, 1.16, 1.20, 1.10)
+
+# nmru for Run 3
+run_3 <- c(1.03, 0.961, 1.30, 1.12, 1.06, 1.10, 0.804, 0.850, 1.02, 1.07, 
+           0.933, 0.979, 1.11, 1.12, 1.02, 1.03, 0.915, 1.02, 0.915, 0.961)
+
+# Variables for run and mouse_id
+run <- factor(c(rep(1, 20), rep(2, 20), rep(3, 20)))
+mouse_id <- factor(c(1:20, 1:20, 1:20))
+
+# Attention: Ln(nmru)
+dat_hb <- data.frame(run=run,
+                     mouse_id=mouse_id,
+                     log_nmru=log(c(run_1, run_2, run_3)))
+
+#-------------------------------------------------------------------------------
+
+pi_c1_gpq_m3 <- function(histdat,
+                      GPQ_n=1e+04,
+                      alpha=0.05){
+  
+  
+  #-----------------------------------------------------------------------------
+  # number of model terms
+  
+  # I
+  I <- length(levels(droplevels(histdat[,1])))
+  
+  # J
+  J <- length(levels(droplevels(histdat[,2])))
+  
+  #-----------------------------------------------------------------------------
+  
+  # modelfit
+  c1_aov <- aov(histdat[,3] ~ histdat[,1] + histdat[,2], data=histdat)
+  
+  #-----------------------------------------------------------------------------
+  
+  # degrees of freedom
+  df <- summary(c1_aov)[[1]][["Df"]]
+  names(df) <- c("a", "b", "res")
+  
+  #-----------------------------------------------------------------------------
+  # sum of sqares
+  ssq <- summary(c1_aov)[[1]][["Sum Sq"]]
+  names(ssq) <- c("a", "b", "res")
+  
+  #-------------------------------------------------------------------------------
+  
+  # GPQ´s for Psi_Squares (step 3)
+  R_Psi_a <- ssq["a"] / rchisq(n=GPQ_n, df=df["a"])
+  R_Psi_b <- ssq["b"] / rchisq(n=GPQ_n, df=df["b"])
+  R_Psi_e <- ssq["res"] / rchisq(n=GPQ_n, df=df["res"])
+  
+  #-----------------------------------------------------------------------------
+  
+  # GPQ for variance components (step 4)
+  
+  # ATTENTION: Some estimates for the variance components can become negative
+  # and are adjusted to be at least 0
+  
+  R_sig2_a_v <- 1/J * (R_Psi_a-R_Psi_e)
+  R_sig2_b_v <- 1/I * (R_Psi_b-R_Psi_e)
+  R_sig2_e_v <- R_Psi_e
+  
+  R_sig2_a <- as.list(replace(R_sig2_a_v, R_sig2_a_v<0, 0))
+  R_sig2_b <- as.list(replace(R_sig2_b_v, R_sig2_b_v<0, 0))
+  R_sig2_e <- as.list(replace(R_sig2_e_v, R_sig2_e_v<0, 0))
+  
+  GPQ_sig2_y <- (Map("+", Map("+", R_sig2_a, R_sig2_b), R_sig2_e))
+  
+  #-----------------------------------------------------------------------------
+  
+  # GPQ for var(mu) (step 5b)
+  GPQ_sig2_mu <- as.list(1/(I*J) * (J*as.numeric(R_sig2_a) +
+                            I*as.numeric(R_sig2_b) + 
+                            as.numeric(R_sig2_e)))
+  
+  #-----------------------------------------------------------------------------
+  
+  # GPQ for var(y_star) (step 5)
+  GPQ_sigma_k <- Map("+", GPQ_sig2_y, GPQ_sig2_mu)
+  
+  # GPQ for the prediction variance
+  GPQ_sigma <- mean(as.numeric(GPQ_sigma_k))
+  
+  # GPQ_D <- qmvnorm(p=1-alpha/2, mean=0, sigma=GPQ_sigma)
+  D_est <- qnorm(p=1-alpha/2, mean=0, sd=sqrt(GPQ_sigma))
+  
+  #-----------------------------------------------------------------------------
+  
+  # PI for 1 future y
+  y_mean <- mean(histdat[,3])
+  L <- y_mean - D_est
+  U <- y_mean + D_est
+  pi_c2 <- c("L"=L, "U"=U)
+  
+  #----------------------------------------------------------------------------
+  
+  # Output
+  output <- pi_c2
+  
+  return(output)
+}
+
+#------------------------------------------------------------------------------
+
+# Interval calculation
+system.time(pi_gpq_m3 <- pi_c1_gpq_m3(histdat=dat_hb))
+
+# Interval on log scale
+pi_gpq_m3
+
+# interval on response scale
+exp(pi_gpq_m3)
+
+# Please note that the distribution of GPQ_sigma_k is right skewed. Hence the
+# estimate GPQ_sigma <- mean(as.numeric(GPQ_sigma_k)) is positively biassed. 
+# E.g. for the interval, that is presented in Table 5 of the paper, the
+# mean(as.numeric(GPQ_sigma_k)) was 0.39887687 and 
+# median(as.numeric(GPQ_sigma_k)) was 0.03150815
+
+
+
---
+++ real_data_example_hoffmann_berger/lmer_pi_c1.R
@@ -0,0 +1,47 @@
+#------------------------------------------------------------------------------
+#------------------- Bootstrap-calibrated PI ----------------------------------
+#------------------------------------------------------------------------------
+
+# Data of Table 4 from Hoffmann & Berger 2011 (Normalized mean RU)
+
+# nmru for Run 1
+run_1 <- c(1.11, 1.00, 1.50, 1.04, 0.929, 1.02, 0.965, 0.973, 1.01, 1.02,
+           1.04, 1.13, 1.03, 1.04, 1.02, 1.04, 0.929, 1.04, 0.947, 0.947)
+
+# nmru for Run 2
+run_2 <- c(1.25, 1.18, 1.52, 1.15, 1.13, 1.03, 1.18, 1.18, 1.21, 1.29,
+           1.16, 1.22, 1.35, 1.28, 1.29, 1.38, 1.20, 1.16, 1.20, 1.10)
+
+# nmru for Run 3
+run_3 <- c(1.03, 0.961, 1.30, 1.12, 1.06, 1.10, 0.804, 0.850, 1.02, 1.07, 
+           0.933, 0.979, 1.11, 1.12, 1.02, 1.03, 0.915, 1.02, 0.915, 0.961)
+
+# Variables for run and mouse_id
+run <- factor(c(rep(1, 20), rep(2, 20), rep(3, 20)))
+mouse_id <- factor(c(1:20, 1:20, 1:20))
+
+# Attention: Ln(nmru)
+dat_hb <- data.frame(run=run,
+                     mouse_id=mouse_id,
+                     log_nmru=log(c(run_1, run_2, run_3)))
+
+
+#------------------------------------------------------------------------------
+
+library(lme4)
+library(predint)
+
+# random effects model with lmer
+fit_hb_lmer <- lmer(log_nmru~1 + (1|run) + (1|mouse_id), data=dat_hb)
+
+# prediction interval for one future observations
+system.time(calib_pi <- lmer_pi(model=fit_hb_lmer, m=1))
+
+# Interval on log scale
+calib_pi
+
+# interval on response scale
+exp(calib_pi[5:6])
+
+
+
